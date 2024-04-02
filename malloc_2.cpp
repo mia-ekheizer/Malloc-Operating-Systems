3393835a11efe4146af97e4647440fe3055d396a
Mia Ekheizer <miaekh12@gmail.com>
2024-04-02 17:12:23 +0300

    malloc_2 passes all tests

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -36,11 +36,10 @@ public:
     }
 
     //for allocation usage
-    void* insert(MallocMetadata* newMetadata, size_t size) {
-        newMetadata->size = size;
+    void* insert(size_t size) {
         MallocMetadata* temp = &dummy;
-        while(temp->next) {
-            if (temp->is_free && temp->size >= newMetadata->size) {
+        while(temp->next || temp->is_free) {
+            if (temp->is_free && temp->size >= size) {
                 temp->is_free = false;
                 num_free_blocks--; 
                 num_free_bytes -= temp->size;
@@ -48,12 +47,17 @@ public:
             }
             temp = temp->next;
         } // no free blocks in the right size found:
+        MallocMetadata* newMetadata = (MallocMetadata*)(sbrk(size + sizeof(MallocMetadata)));
+        if(newMetadata == (void*)(-1))
+            return nullptr;
+
         temp->next = newMetadata;
         newMetadata->prev = temp;
         newMetadata->next = nullptr;
         newMetadata->is_free = false;
+        newMetadata->size = size;
         num_used_blocks++;
-        num_used_bytes += newMetadata->size;
+        num_used_bytes += size;
         return newMetadata + 1;
     }
 
@@ -73,11 +77,7 @@ void* smalloc(size_t size) {
     if(size == 0 || size > 1e8)
         return nullptr;
 
-    MemoryList::MallocMetadata* newMetadata = (MemoryList::MallocMetadata*)(sbrk(size + sizeof(MemoryList::MallocMetadata)));
-    if(newMetadata == (void*)(-1))
-        return nullptr;
-
-    return memList.insert(newMetadata, size);
+    return memList.insert(size);
 }
 
 void* scalloc(size_t num, size_t size) {
@@ -111,7 +111,7 @@ void* srealloc(void* oldp, size_t size) {
     if (size <= ((MemoryList::MallocMetadata*)oldp - 1)->size)
         return oldp;
 
-    void* newAddress = memList.insert((MemoryList::MallocMetadata*)oldp - 1, size);
+    void* newAddress = memList.insert(size);
     newAddress = memmove(newAddress, oldp, ((MemoryList::MallocMetadata*)oldp - 1)->size);
     sfree(oldp);
     return newAddress;
